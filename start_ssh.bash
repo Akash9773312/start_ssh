e330e635c3757f0f92099dae1770ac58c3ab3459
Akash9773312 <am9773312@outlook.com>
2023-07-28 17:11:32 +0530

    start_ssh updated

--- start_ssh.bash
+++ start_ssh.bash
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 sleep 50
+
 start_tunnel() {
-  ssh -N -R 7624:localhost:22 rkssh@rkssh.rev.vet
+  ssh -o ExitOnForwardFailure=yes -N -R 7624:localhost:22 rkssh@rkssh.rev.vet
 }
 
 check_tunnel() {
   ssh -q -o "BatchMode=yes" -O check -p 7624 rkssh@rkssh.rev.vet
 }
 
+check_internet() {
+  while true; do
+    if ping -c 1 google.com &> /dev/null; then
+      echo "Internet is available."
+      break
+    else
+      echo "Internet connection is lost. Checking again in 5 seconds..."
+      sleep 5
+    fi
+  done
+}
+
 while true; do
+  check_internet
+
   if ! check_tunnel; then
     echo "SSH tunnel is not active. Restarting in 5 seconds..."
     sleep 5
